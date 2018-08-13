a34f7a7f38c11ab724940208a130afd274bb55fb
coderuz <mitazimjon@gmail.com>
2018-08-13 15:18:35 +0500

    Min funtion created

---
+++ min.go
@@ -0,0 +1,17 @@
+package min
+
+func main(){
+  println(Min(1,2,3))
+  println(Min(2,3,1))
+  println(Min(3,2,1))
+}
+
+func Min(nums ...int) int {
+   min := nums[0]
+   for _, num := range nums{
+      if num < min{
+         min = num
+      }
+   }
+   return min
+}
